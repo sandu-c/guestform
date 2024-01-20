1860db9985157cff966819f942c61763d2693e76
Alex C <raba.ax@gmail.com>
2024-01-20 17:23:07 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,10 +8,29 @@ module.exports = withCSS({
 });
 
 const repo = 'sandu-c'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
 module.exports = {
   assetPrefix: assetPrefix,
   basePath: basePath,
 }
+
+// module.exports = {
+//   // …
+//   images: {
+//     loader: 'imgix',
+//     path: 'the "domain" of your Imigix source',
+//   },
+// }
