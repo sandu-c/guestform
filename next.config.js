84059fa81fd24ff4f268f9db9a206b7be94c81a5
Alex <raba.ax@gmail.com>
2024-03-03 02:06:28 +0100

    attempt to make asset routes work

--- next.config.js
+++ next.config.js
@@ -12,13 +12,13 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = "/";
 let basePath = "/";
 
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\/.*/, "/");
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\/.*/, "/");
 
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}/`;
-}
+//   assetPrefix = `/${repo}/`;
+//   basePath = `/${repo}/`;
+// }
 
 module.exports = {
   // Your other configurations...
