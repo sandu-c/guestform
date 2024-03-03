00d8149981c5863411acf2ffc379cede1cc2be3f
Alex <raba.ax@gmail.com>
2024-03-03 02:13:41 +0100

    attempt to make asset routes work

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const repo = "/";
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 let assetPrefix = "/";
-let basePath = "/";
+let basePath = "";
 
 // if (isGithubActions) {
 //   // trim off `<owner>/`
