7dd8339bcb30a8f2780fc0d14f1a56bc060811c9
Alex <raba.ax@gmail.com>
2024-03-03 01:01:51 +0100

    fixing asset resources

--- next.config.js
+++ next.config.js
@@ -9,16 +9,16 @@
 const repo = "guestform";
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = "";
+let assetPrefix = "guestform";
 let basePath = "";
 
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
+//   assetPrefix = `/${repo}/`;
+//   basePath = `/${repo}`;
+// }
 
 module.exports = {
   // Your other configurations...
